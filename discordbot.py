bd647b3c2e90d43d6d3caf38a9b6a71c40217520
taku0224 <79884495+taku0224@users.noreply.github.com>
2021-03-02 11:12:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def hello(ctx):
+    await ctx.send('こんにちは')
 
 
 bot.run(token)

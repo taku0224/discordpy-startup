85d622a62b876cbd7b01b13a25ba20c9a0e27e9f
taku0224 <79884495+taku0224@users.noreply.github.com>
2021-03-02 17:09:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,6 +11,15 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
+ @client.event
+async def on_ready():
+    print('Ready')
+
+@client.command(pass_context=True)
+async def join(ctx, *, question):
+    question_message = await ctx.send(f'アンケート： {question}\nhoi4参加できる？')
+    await question_message.add_reaction('✅')
 
 
 @bot.command()

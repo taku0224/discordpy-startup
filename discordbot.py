682f305f73e274fc3c0a0a7d69e61bc678411784
taku0224 <79884495+taku0224@users.noreply.github.com>
2021-03-02 12:40:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,5 +22,15 @@ async def think(ctx):
     await ctx.send(':thinking:')
     
 
+if message.content == "join":
+    if message.author.voice is None:
+        await message.channel.send("あなたはボイスチャンネルに接続していません。")
+        return
+    # ボイスチャンネルに接続する
+    await message.author.voice.channel.connect()
+
+    await message.channel.send("接続しました。")
+    
+
 pip install discord.py[voice]
 bot.run(token)
